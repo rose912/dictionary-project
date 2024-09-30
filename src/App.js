974fabbf18bc9d22e9de0759985852c513f260f7
rose912 <chioma.osuagwu@ymail.com>
2024-09-30 12:46:30 -0400

    Added search engine and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo"
-        alt="logo img-fluid" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo img-fluid" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>coded by Rosemary Obioma</small>
+        </footer>
+      </div>
     </div>
   );
 }
