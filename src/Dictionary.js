b6271c5efb829770660f6ca19579c2e097aed69b
rose912 <chioma.osuagwu@ymail.com>
2024-10-02 11:14:08 -0400

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,24 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
-  function search(event) {
-    event.preventDefault();
-    alert(`searching for ${keyword} definition`);
+  function handleResponse(response) {
+    console.log(response.data[0]);
   }
 
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value)
+  function search(event) {
+    event.preventDefault();
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+
+ function handleKeywordChange(event) {
+    setKeyword(event.target.value);
   }
 
   return (
